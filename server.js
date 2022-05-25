191fc72026a6000a91bbb771a16c0a318ed44eab
abhiroop99z <abhiroop.b@zensar.com>
2022-05-25 18:23:06 +0530

    cors, nodemon added

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const url = 'mongodb://localhost/NFT'
-const app = express()
-mongoose.connect(url,err=>{
-    //console.log('check1')
-    if(err){
-        //console.log('check2')
-        console.log('Error!'+err)
-    }else{
-        console.log('Connected to mongodb')
-    }
-})
-app.use(express.json())
-const api = require('./Routes/api')
-app.use('/api',api)
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const url = "mongodb://localhost/NFT";
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+mongoose.connect(url, (err) => {
+  //console.log('check1')
+  if (err) {
+    //console.log('check2')
+    console.log("Error!" + err);
+  } else {
+    console.log("Connected to mongodb");
+  }
+});
+app.use(express.json());
+const api = require("./Routes/api");
+app.use("/api", api);
 
-app.listen(9000, function(){
-    console.log('Server Started')
-})
+app.listen(9000, function () {
+  console.log("Server Started");
+});
