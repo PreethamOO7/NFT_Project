206ac6b6d1a7509bd56416defa9f36e6a2a83fc5
abhiroop99z <abhiroop.b@zensar.com>
2022-05-30 09:08:43 +0530

    email validation

--- Models/NFT.js
+++ Models/NFT.js
@@ -1,25 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const NFTSchema = new mongoose.Schema({
-    email : String,
-    public_key : String,
-    OTP : String,
-    NFTdetails: {
-        tokenId: Number,
-        tokenURI: String,
-        imageurl: String,
-        name: String,
-        attributes: Array
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: "Please enter a valid email",
     },
-    Delivery_address: {
-        Name : String,
-        address1: String,
-        address2: String,
-        City: String,
-        Country: String,
-        ZipCode: Number,
-        Phone_Number: Number
-    }
-})
+    required: [true, "Email required"],
+  },
+  public_key: String,
+  OTP: String,
+  NFTdetails: {
+    tokenId: Number,
+    tokenURI: String,
+    imageurl: String,
+    name: String,
+    attributes: Array,
+  },
+  Delivery_address: {
+    Name: String,
+    address1: String,
+    address2: String,
+    City: String,
+    Country: String,
+    ZipCode: Number,
+    Phone_Number: Number,
+  },
+});
 
-module.exports = mongoose.model('NFT',NFTSchema)
+module.exports = mongoose.model("NFT", NFTSchema);
